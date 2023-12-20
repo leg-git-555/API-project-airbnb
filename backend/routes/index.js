caadeb9b470db665a08ac7e568312e4e83dc1a2e
Nicholas Leger <nick.leger2314@gmail.com>
2023-12-20 17:18:53 -0500

    phase1 edit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,10 @@ const router = express.Router();
 //   res.send('Hello World!');
 // });
 
+const apiRouter = require('./api')
+
+router.use('/api', apiRouter)
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -14,5 +18,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+
 
 module.exports = router;
