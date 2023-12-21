e83b83ff709a67322bda39d85ddd43b257894cf2
Nicholas Leger <nick.leger2314@gmail.com>
2023-12-21 18:16:41 -0500

    seeder file work. validations works

---
+++ backend/db/seeders/20231221185708-demo-user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+      await User.bulkCreate([
+        {
+          username: 'Bukayo-7',
+          email: 'bukayo_saka@user.io',
+          hashedPassword: bcrypt.hashSync('password')
+        },
+        {
+          username: 'Declan-41',
+          email: 'declan_rice@user.io',
+          hashedPassword: bcrypt.hashSync('password2')
+        },
+        {
+          username: 'William-2',
+          email: 'william_saliba@user.io',
+          hashedPassword: bcrypt.hashSync('password3')
+        },
+
+      ], { validate: true })
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users'
+    const Op = Sequelize.Op
+
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: [
+        'Bukayo-7', 'Declan-41', 'William-2'
+      ]}
+    }, {})
+  }
+};
