ecea4ad4b5d1239f55e6b0bfe7c463a36ae03b9b
Nicholas Leger <nick.leger2314@gmail.com>
2023-12-21 18:43:21 -0500

    added a default scope to the User model to improve security

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 
+const {User} = require('../../db/models')
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
+router.get('/users', async (req, res) => {
+    const users = await User.findAll()
+
+    res.json(users)
+})
+
 // router.get('/woah', (req, res) => {
-//     res.send('woahhhh')
+//     res.send(`woahhh you're doing it!`)
 // })
   
 
