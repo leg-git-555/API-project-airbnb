640d328bcd5d7577664000d17ff601df03ce2483
Nicholas Leger <nick.leger2314@gmail.com>
2023-12-20 19:01:21 -0500

    bug when running migration file. need to fix tomorrow

---
+++ backend/db/migrations/20231220231053-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+// require('dotenv').config()
+
+// let options = {};
+
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
+
+const { SELECT } = require('sequelize/types/query-types');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIME')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIME')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    // options.tableName = 'Users'
+    await queryInterface.dropTable('Users'); //may want to await this
+  }
+};
