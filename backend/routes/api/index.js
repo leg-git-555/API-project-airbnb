e6a891d3cbef68773a0778a9af89544248d35bbb
Nicholas Leger <nick.leger2314@gmail.com>
2023-12-22 13:54:04 -0500

    user authentication working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,24 +1,28 @@
 const router = require('express').Router();
-const { Op } = require('sequelize')
-
 const {User} = require('../../db/models')
-const { setTokenCookie, restoreUser } = require('../../utils/auth.js');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 
 router.use(restoreUser)
 
+
+
+
+  
+  
+
+/*
+
+//  USER AUTH TEST ROUTES BELOW
+
 router.get('/restore-user', (req, res) => {
+      return res.json(req.user);
+    }
+  );
+
+router.get('/require-auth', requireAuth, (req, res) => {
     return res.json(req.user);
   }
 );
-
-
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
-
-
-
-// TEST ROUTES BELOW
 
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
@@ -30,15 +34,8 @@ router.get('/set-token-cookie', async (_req, res) => {
   return res.json({ user: user });
 });
 
-// router.get('/users', async (req, res) => {
-//     const users = await User.findAll()
+*/
 
-//     res.json(users)
-// })
 
-// router.get('/woah', (req, res) => {
-//     res.send(`woahhh you're doing it!`)
-// })
   
-
 module.exports = router
