e4cd11719f92d0f378aaaa65a2435017602beaf2
Nicholas Leger <nick.leger2314@gmail.com>
2024-01-01 15:59:48 -0500

    working on spots endpoints

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const { setTokenCookie } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { Spot, Review, SpotImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth')
 
 const router = express.Router();
 
@@ -9,8 +10,31 @@ router.get('/', async (req, res) => {
     const Spots = await Spot.findAll()
     let resObj = {Spots}
 
-    res.json(resObj)
+    return res.json(resObj)
 
+})
+
+router.get('/current', requireAuth,  async (req, res) => {
+    const {user} = req;
+    let id = user.id;
+    
+    let userSpots = await Spot.findAll({
+        where: {
+            ownerId: id
+        }, 
+    })
+
+    let userSpotsB = userSpots.map(spot => spot.toJSON())
+
+    console.log(userSpotsB)
+
+    for (let spot of userSpotsB) {
+        console.log(spot) //add avg score in here 
+        spot.testKey = 'test value'
+    }
+
+
+    return res.json(userSpotsB)
 })
 
 
