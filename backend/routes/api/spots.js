b09d57ccbc6171d2b84585f47ff4b2fd7e8108b0
Nicholas Leger <nicholasleger4@gmail.com>
2024-01-07 00:18:53 -0500

    debug then refactor create a booking

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -612,28 +612,6 @@ const validateSpot = [
         endDate = new Date(endDate)
         let endSeconds = endDate.getTime()
 
-
-
-                //error handler 3 -- start date in past
-                if (!startSeconds || (startSeconds < now)) {
-                    return res.status(400).json({
-                        "message": "Bad Request",
-                        "errors": {
-                        "startDate": "startDate cannot be in the past"
-                        }
-                    })
-                }
-
-                //error handler 4 -- end date before start date
-                if (!endDate || (endSeconds <= startSeconds)) {
-                    return res.status(400).json({
-                        "message": "Bad Request",
-                        "errors": {
-                            "endDate": "endDate cannot be on or before startDate"
-                        }
-                    })
-                }
-
         let bookings = await Booking.findAll({
             where: {
                 spotId
@@ -645,40 +623,37 @@ const validateSpot = [
             for (let booking of bookings) {
                 let currStartSeconds = new Date(booking.startDate).getTime()
                 let currEndSeconds = new Date(booking.endDate).getTime()
+
+                    let tracker = 0;
+                    let errorObj = {
+                        "message": "Sorry, this spot is already booked for the specified dates",
+                        "errors": {
+
+                        }
+                      }
                 
                     //check start date conflict
                     if (startSeconds >= currStartSeconds && startSeconds <= currEndSeconds) {
-                        return res.status(403).json({
-                            "message": "Sorry, this spot is already booked for the specified dates",
-                            "errors": {
-                              "startDate": "Start date conflicts with an existing booking"
-                            }
-                          })
+                        tracker++
+                        errorObj.errors.startDate = "Start date conflicts with an existing booking"
                     }
 
                     //check end date conflict
                     if (endSeconds >= currStartSeconds && endSeconds <= currEndSeconds) {
-                        return res.status(403).json({
-                            "message": "Sorry, this spot is already booked for the specified dates",
-                            "errors": {
-                                "endDate": "End date conflicts with an existing booking"
-                            }
-                          })
+                        tracker++
+                        errorObj.errors.endDate = "End date conflicts with an existing booking"
                     }
 
-                    let wrapCheckStart = currEndSeconds >= startSeconds && currEndSeconds <= endSeconds
-
-                    //check that new date don't wrap around an existing booking
-                    if (wrapCheckStart) {
-                        return res.status(403).json({
-                            "message": "Sorry, this spot is already booked for the specified dates",
-                            "errors": {
-                              "startDate": "Start date conflicts with an existing booking",
-                              "endDate": "End date conflicts with an existing booking"
-                            }
-                          })
+                    //check surroud
+                    if (startSeconds < currStartSeconds && endSeconds > currEndSeconds) {
+                        tracker++
+                        errorObj.errors.startDate = "Start date conflicts with an existing booking"
+                        errorObj.errors.endDate = "End date conflicts with an existing booking"
                     }
 
+                    if (tracker > 0) {
+                        return res.status(403).json(errorObj)
+                    }
             }
 
         let newBooking = await Booking.create({spotId, userId, startDate, endDate})
