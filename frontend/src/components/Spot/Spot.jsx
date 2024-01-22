7e8a39021d2a3ac18d2c71da805453ead6d0ba92
Nicholas Leger <nicholasleger4@gmail.com>
2024-01-22 12:48:48 -0500

    add callout box to spot page

--- frontend/src/components/Spot/Spot.jsx
+++ frontend/src/components/Spot/Spot.jsx
@@ -5,12 +5,12 @@ import { useParams } from "react-router-dom"
 import './Spot.css'
 
 
-export function Spot () {
+export function Spot() {
     const dispatch = useDispatch()
-    const {spotId} = useParams()
-    const {spot} = useSelector(state => state.spot)
+    const { spotId } = useParams()
+    const { spot } = useSelector(state => state.spot)
     let spotImageRay = spot.SpotImages //without coniditional in return statement, page breaks
-    
+
 
     useEffect(() => {
         dispatch(getSpotByIdThunk(spotId))
@@ -22,16 +22,30 @@ export function Spot () {
             <h2>{spot.name}</h2>
             <h3>{`${spot.city}, ${spot.state}, ${spot.country}`}</h3>
             <div className='singleSpotImageContainer'>
-                <img src={spotImageRay && spotImageRay[0].url}></img> 
-                <div className ='singleSpotImageContainerB'>
                 <img src={spotImageRay && spotImageRay[0].url}></img>
-                <img src={spotImageRay && spotImageRay[0].url}></img>
-                <img src={spotImageRay && spotImageRay[0].url}></img>
-                <img src={spotImageRay && spotImageRay[0].url}></img>
+                <div className='singleSpotImageContainerB'>
+                    <img src={spotImageRay && spotImageRay[0].url}></img>
+                    <img src={spotImageRay && spotImageRay[0].url}></img>
+                    <img src={spotImageRay && spotImageRay[0].url}></img>
+                    <img src={spotImageRay && spotImageRay[0].url}></img>
                 </div>
             </div>
-            <div className='singleSpotHostInfo'>
-                <h3>{`Hosted by ${spot.Owner?.firstName} ${spot.Owner?.lastName}`}</h3>
+            <div className='singleSpotBottomThird'>
+                <div className='hostInfoSpotDescription'>
+                    <h3>{`Hosted by ${spot.Owner?.firstName} ${spot.Owner?.lastName}`}</h3>
+                    <p>{spot?.description}</p>
+                </div>
+                <div className="calloutBox">
+                    <div>
+                        <div>
+                        {`$${spot?.price} night`}
+                        </div>
+                        <div>
+                            star content here
+                        </div>
+                    </div>
+                    <button>Reserve</button>
+                </div>
             </div>
         </div>
     )
