0aa89664cc63f3e3b786d82132ba345ba7704149
Nicholas Leger <nicholasleger4@gmail.com>
2024-01-28 17:02:58 -0500

    change openModal inner element from li to div

--- frontend/src/components/Navigation/OpenModalMenuItem.jsx
+++ frontend/src/components/Navigation/OpenModalMenuItem.jsx
@@ -15,7 +15,8 @@ function OpenModalMenuItem({
   };
 
   return (
-    <li onClick={onClick}>{itemText}</li>
+    <div onClick={onClick}>{itemText}</div>
+    // <li onClick={onClick}>{itemText}</li>
   );
 }
 
